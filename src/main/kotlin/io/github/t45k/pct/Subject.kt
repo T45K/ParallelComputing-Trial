1675aeec2209f09dcba9f292d603e09f41bdd862
T45K <tasktas9@gmail.com>
2020-03-23 17:34:58 +0900

    add new subject

--- src/main/kotlin/io/github/t45k/pct/Subject.kt
+++ src/main/kotlin/io/github/t45k/pct/Subject.kt
@@ -4,8 +4,10 @@ import java.util.ArrayList
 import java.util.stream.Collectors
 import kotlin.math.sqrt
 
-// エラトステネスの篩
-// 省計算量
+/**
+ * エラトステネスの篩
+ * 計算量はO(n log(log(n)))
+ */
 fun sieveOfEratosthenes(number: Int): List<Int> {
     val sqrt = sqrt(number.toDouble()).toInt()
     var numbers: List<Int> = (2..number).toList()
@@ -23,11 +25,24 @@ fun sieveOfEratosthenes(number: Int): List<Int> {
     return primeNumbers
 }
 
-fun sumAccumulation(number: Int): Long {
-    var sum = 0L
-    for (i in 1..number) {
-        sum += i
-    }
-    return sum
-}
+/**
+ * 累積和
+ * 計算量は線形
+ */
+fun sumAccumulation(number: Int): Long = (1..number.toLong()).sum()
 
+/**
+ * 与えられた数字から3つ選んだ組み合わせの和の総数
+ * O(n ^ 3)
+ */
+fun sumAccumulation2(number: Int): Long = (1..number.toLong())
+        .map { i ->
+            (i + 1..number.toLong())
+                    .map { j ->
+                        (j + 1..number.toLong())
+                                .map { k -> i + j + k }
+                                .sum()
+                    }
+                    .sum()
+        }
+        .sum()
