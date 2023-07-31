418d7144db2f588c734d755e2dd41c56d9762565
T45K <tasktas9@gmail.com>
2023-07-31 22:32:47 +0900

    create many coroutines https://kotlinlang.org/docs/coroutines-basics.html#coroutines-are-light-weight

--- src/main/kotlin/io/github/t45k/pct/coroutine/doc/CoroutinesBasics.kt
+++ src/main/kotlin/io/github/t45k/pct/coroutine/doc/CoroutinesBasics.kt
@@ -1,25 +1,14 @@
 package io.github.t45k.pct.coroutine.doc
 
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-// Sequentially executes doWorld followed by "Done"
 fun main() = runBlocking {
-    doWorld()
-    println("Done")
-}
-
-// Concurrently executes both sections
-suspend fun doWorld() = coroutineScope { // this: CoroutineScope
-    launch { // ここの launch と
-        delay(2000L)
-        println("World 2")
+    repeat(50_000) { // launch a lot of coroutines
+        launch {
+            delay(5000L)
+            print(".")
+        }
     }
-    launch { // ここの launch は並行に走る
-        delay(1000L)
-        println("World 1")
-    }
-    println("Hello")
 }
