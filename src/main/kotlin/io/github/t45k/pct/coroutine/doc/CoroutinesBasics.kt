2e13473a9dc024433ce2c54651b4fd7f311fd9a0
T45K <tasktas9@gmail.com>
2023-07-31 21:48:43 +0900

    use scope builder https://kotlinlang.org/docs/coroutines-basics.html#scope-builder

--- src/main/kotlin/io/github/t45k/pct/coroutine/doc/CoroutinesBasics.kt
+++ src/main/kotlin/io/github/t45k/pct/coroutine/doc/CoroutinesBasics.kt
@@ -1,15 +1,18 @@
 package io.github.t45k.pct.coroutine.doc
 
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking { // this: CoroutineScope
-    launch { doWorld() }
-    println("Hello from " + Thread.currentThread().name)
+    doWorld()
 }
 
-private suspend fun doWorld() {
-    delay(1000L)
-    println("World! from " + Thread.currentThread().name)
+private suspend fun doWorld() = coroutineScope {
+    launch {
+        delay(1000L)
+        println("World! from " + Thread.currentThread().name)
+    }
+    println("Hello from " + Thread.currentThread().name)
 }
