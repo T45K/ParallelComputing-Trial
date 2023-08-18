21f3e27b213162373de54844deddc397e7442945
T45K <tasktas9@gmail.com>
2023-08-18 23:15:03 +0900

    lazy https://kotlinlang.org/docs/composing-suspending-functions.html#lazily-started-async

--- src/main/kotlin/io/github/t45k/pct/coroutine/doc/ComposingSuspendingFunction.kt
+++ src/main/kotlin/io/github/t45k/pct/coroutine/doc/ComposingSuspendingFunction.kt
@@ -1,15 +1,45 @@
 package io.github.t45k.pct.coroutine.doc
 
+import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import kotlin.system.measureTimeMillis
 
 fun main() = runBlocking {
+    lazyAsync()
+}
+
+// also 1 sec
+fun lazyAsync() = runBlocking {
+    val time = measureTimeMillis {
+        val one = async(start = CoroutineStart.LAZY) { doSomethingUsefulOne() }
+        val two = async(start = CoroutineStart.LAZY) { doSomethingUsefulTwo() }
+        one.start()
+        two.start()
+        println("The answer is ${one.await() + two.await()}")
+    }
+
+    println(time)
+}
+
+// 1 sec
+fun async() = runBlocking {
     val time = measureTimeMillis {
         val one = async { doSomethingUsefulOne() }
         val two = async { doSomethingUsefulTwo() }
         println("The answer is ${one.await() + two.await()}")
+    }
+
+    println(time)
+}
+
+// 2 sec
+fun sequential() = runBlocking {
+    val time = measureTimeMillis {
+        val one = doSomethingUsefulOne()
+        val two = doSomethingUsefulTwo()
+        println("The answer is ${one + two}")
     }
 
     println(time)
